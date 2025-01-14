274632fd656de6c8150aad657e8de096f9489688
PRADEEPS-PC\talkw <pradeep@retailbrige.org>
2025-01-14 05:57:36 -0500

    gloss finish changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ObjectiveComponent} from './components/objective/objective.component';
 import {ContactUsComponent} from './components/contact-us/contact-us.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ObjectiveComponent, ContactUsComponent],
+  imports: [RouterOutlet, ObjectiveComponent, ContactUsComponent, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
